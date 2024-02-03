60f7a07e087ed7dca8375861935faf48c2b582a5
zengjindong110“‘“ <17786451825@163.com>
2024-02-03 20:44:58 +0800

    基本内容

---
+++ xuecheng-plus-message/src/main/java/com/xuecheng/XuechengPlusMessageApplication.java
@@ -0,0 +1,13 @@
+package com.xuecheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuechengPlusMessageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XuechengPlusMessageApplication.class, args);
+    }
+
+}
---
+++ xuecheng-plus-search/src/main/java/com/xuecheng/XuechengPlusSearchApplication.java
@@ -0,0 +1,13 @@
+package com.xuecheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuechengPlusSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XuechengPlusSearchApplication.class, args);
+    }
+
+}
